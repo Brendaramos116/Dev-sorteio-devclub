af8ed4c7b79c2c3882eff7189391f2c73f102bca
Brendaramos116 <brendaramos116@hotmail.com>
2025-07-02 23:36:58 -0300

    Arrumando erros de digitação e estilização

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function generateNumber(){
     const  max = Math.floor(document.querySelector(".input-left").value)
     const result = Math.floor(Math.random() * (max - min + 1)) + min
 
-    alert("Seu núemro sorteado é: " + result)
+    alert("Seu número sorteado é: " + result)
 
     document.querySelector(".input-right").value = ""
     document.querySelector(".input-left").value = ""
